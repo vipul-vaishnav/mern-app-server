af900fd635e4623cad067a66f8585b4e22ab1e2e
Vipul <007vipulrank1@gmail.com>
2023-02-12 16:18:01 +0530

    INIT: express app and setup cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
 
-const PORT = 5000
+dotenv.config({})
+
+const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
 
-app.listen(PORT, function () {
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the Express app')
+})
+
+app.get('/json-data', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    data: [
+      { name: 'Vipul', age: 22 },
+      { name: 'Rahul', age: 24 },
+      { name: 'Sarthak', age: 21 }
+    ]
+  })
+})
+
+app.listen(PORT, () => {
   console.log('APP listening on port', PORT)
 })
